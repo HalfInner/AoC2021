9bcb2af43aca22366611ceaffa40895ef0a830f8
Kajetan Brzuszczak <Kajetan.Brzuszczak@Cybercom.com>
2021-12-16 18:37:23 +0100

    d16.pending

--- d16/d16.go
+++ d16/d16.go
@@ -36,26 +36,64 @@ func read_data() Data {
 	return data
 }
 
-func BitReverse32(x uint32) uint32 {
-	x = (x&0x55555555)<<1 | (x&0xAAAAAAAA)>>1
-	x = (x&0x33333333)<<2 | (x&0xCCCCCCCC)>>2
-	x = (x&0x0F0F0F0F)<<4 | (x&0xF0F0F0F0)>>4
-	x = (x&0x00FF00FF)<<8 | (x&0xFF00FF00)>>8
-	return (x&0x0000FFFF)<<16 | (x&0xFFFF0000)>>16
+type BitReaderStack struct {
+	bit_cnt, bit_len int
+}
+
+type Stack struct {
+	capacity int
+	vec      []BitReaderStack
+}
+
+func (s *Stack) Size() int {
+	return s.capacity
+}
+func (s *Stack) Empty() {
+	s.capacity = 0
+}
+
+func (s *Stack) Push(value BitReaderStack) {
+	if s.capacity == len(s.vec) {
+		s.vec = append(s.vec, value)
+	} else {
+		s.vec[s.capacity] = value
+	}
+	s.capacity++
+}
+
+func (s *Stack) Pop() {
+	if s.capacity == 0 {
+		log.Panic("Trying to pop from empty stack")
+	}
+	s.capacity--
+}
+
+func (s *Stack) Top() BitReaderStack {
+	if s.capacity == 0 {
+		log.Panic("Trying to read from empty stack")
+	}
+	return s.vec[s.capacity-1]
 }
 
 type BitReader struct {
-	hex_transmission string
-	bit_cnt, bit_len int
+	hex_transmission           string
+	bit_cnt, bit_len, vers_cnt int
+	stack                      Stack
 }
 
 func (br *BitReader) init(hex_transmission string) {
 	br.hex_transmission = hex_transmission
 	br.bit_cnt = 0
 	br.bit_len = len(br.hex_transmission) * 4
+	br.vers_cnt = 0
+	br.stack.Push(BitReaderStack{br.bit_cnt, br.bit_len})
 }
 
 func (br *BitReader) read_hex(bits int) int {
+	if br.bit_cnt+bits >= br.bit_len {
+		br.bit_cnt += bits
+		return 0
+	}
 	val := 0
 	read_bits := 0
 	for read_bits < bits {
@@ -89,8 +127,13 @@ func (br *BitReader) read_hex(bits int) int {
 	return val
 }
 
+func (br *BitReader) get_vers_cnt() int {
+	return br.vers_cnt
+}
 func (br *BitReader) read_version() int {
-	return br.read_hex(3)
+	ver := br.read_hex(3)
+	br.vers_cnt += ver
+	return ver
 }
 
 func (br *BitReader) read_type() int {
@@ -107,7 +150,7 @@ func (br *BitReader) read_value() int {
 		}
 		val <<= 4
 	}
-	br.align()
+	// br.align()
 	return val
 }
 
@@ -119,21 +162,103 @@ func (br *BitReader) align() {
 }
 
 func (br *BitReader) nothing_to_read() bool {
-	return br.bit_len == br.bit_cnt
+	return br.bit_cnt >= br.bit_len
+}
+
+func (br *BitReader) call_bit_reader_def() {
+	br.call_bit_reader(br.bit_cnt)
+}
+
+func (br *BitReader) call_bit_reader(len int) {
+	br.stack.Push(BitReaderStack{br.bit_cnt, br.bit_len})
+	br.bit_len = br.bit_cnt + len
+}
+
+func (br *BitReader) return_bit_reader() int {
+	cnt := br.bit_cnt
+	br.bit_cnt, br.bit_len = br.stack.Top().bit_cnt, br.stack.Top().bit_len
+	br.stack.Pop()
+	return cnt
+}
+
+func (br *BitReader) read_operator() {
+	switch br.read_hex(1) {
+	case 0: // sum
+		log.Print("Operator bits")
+		length_in_bits := br.read_hex(15)
+		br.call_bit_reader(length_in_bits)
+		br.read_packets()
+		br.bit_cnt = br.return_bit_reader()
+	case 1:
+		log.Print("Operator packets")
+		number_of_packets := br.read_hex(11)
+		for packet := 0; packet < number_of_packets; packet++ {
+			br.read_packets()
+		}
+	case 2:
+		log.Print("Operator packets")
+		number_of_packets := br.read_hex(11)
+		for packet := 0; packet < number_of_packets; packet++ {
+			br.read_packets()
+		}
+	case 3:
+		log.Print("Operator packets")
+		number_of_packets := br.read_hex(11)
+		for packet := 0; packet < number_of_packets; packet++ {
+			br.read_packets()
+		}
+	case 4:
+		log.Print("Operator packets")
+		number_of_packets := br.read_hex(11)
+		for packet := 0; packet < number_of_packets; packet++ {
+			br.read_packets()
+		}
+	case 5:
+		log.Print("Operator packets")
+		number_of_packets := br.read_hex(11)
+		for packet := 0; packet < number_of_packets; packet++ {
+			br.read_packets()
+		}
+	case 6:
+		log.Print("Operator packets")
+		number_of_packets := br.read_hex(11)
+		for packet := 0; packet < number_of_packets; packet++ {
+			br.read_packets()
+		}
+	case 7:
+		log.Print("Operator packets")
+		number_of_packets := br.read_hex(11)
+		for packet := 0; packet < number_of_packets; packet++ {
+			br.read_packets()
+		}
+	}
+}
+
+func (br *BitReader) read_packets() {
+	for !br.nothing_to_read() {
+		ver := br.read_version()
+		log.Printf("Read Version: %d", ver)
+		packet_type := br.read_type()
+		switch packet_type {
+		case 4: // value
+			val := br.read_value()
+			log.Printf("Read Value: %d", val)
+		default: // operator
+			br.read_operator()
+		}
+	}
+}
+
+func (br *BitReader) start() {
+	br.read_packets()
 }
 
 func d16_1(data Data) int {
 	defer aoc_fun.Track(aoc_fun.Runningtime())
 	var br BitReader
 	br.init(data.transmission)
-	br.read_version()
-	type_ := br.read_type()
-	literal_value_id := 4
-	if type_ == literal_value_id {
-		return br.read_value()
-	}
-
-	return -1
+	br.start()
+	return br.get_vers_cnt()
 }
 
 func d16_2(data Data) int {
